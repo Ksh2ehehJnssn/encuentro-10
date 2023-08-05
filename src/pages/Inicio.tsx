db8b3c9b77c7ac8cb3d3cdea610c6d6701addcd5
Pedro Jossias Rivera Lopez <est.priveral@uml.edu.ni>
2023-08-05 12:44:01 -0600

    useEfecct

--- src/pages/Inicio.tsx
+++ src/pages/Inicio.tsx
@@ -1,6 +1,15 @@
 import { Title } from '../components/Title';
+import React, { useState, useEffect } from 'react';
+
+
 
 export const Inicio = () => {
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+  });
+
   return (
     <>
       <Title titulo="Bienvenido!" />
